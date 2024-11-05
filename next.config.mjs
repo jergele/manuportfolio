b1b7ee39e41976fea6cf031804575f7c71afef6a
[manualastalo] <jealasta@uef.fi>
2024-11-05 20:08:07 +0200

    lisätään

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    // Use basePath only in production (GitHub Pages)
-    basePath: process.env.NODE_ENV === 'production' ? '/manuportfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/manuportfolio/' : '',
-    // Disable source maps in production
-    productionBrowserSourceMaps: false,
-    distDir: 'dist',
-    trailingSlash: true,
-    skipTrailingSlashRedirect: true,
-    // Add this to ensure all chunks are included
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-          net: false,
-          tls: false,
-        };
-      }
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          minSize: 20000,
-          maxSize: 244000,
-          cacheGroups: {
-            default: false,
-            vendors: false,
-            commons: {
-              name: 'commons',
-              chunks: 'all',
-              minChunks: 2,
-            },
-          },
-        },
-      };
-      return config;
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  distDir: ".next",
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
