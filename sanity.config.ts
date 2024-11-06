255b727bb75629260afde53277fcde0babc46b05
[manualastalo] <jealasta@uef.fi>
2024-11-06 09:02:53 +0200

    uusi rakenne23

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
-import { schema } from "./src/sanity/schemaTypes";
+import { schemaTypes } from "./src/sanity/schemaTypes";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "i3l7154n";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
@@ -15,5 +15,7 @@ export default defineConfig({
   dataset,
   // Remove basePath when deploying to sanity.studio
   plugins: [deskTool(), visionTool()],
-  schema,
+  schema: {
+    types: schemaTypes,
+  },
 });
